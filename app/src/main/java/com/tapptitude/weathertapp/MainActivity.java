c6c56f13e2dee9ea9ea56a07ddccc6c1bded9841
Ambrozie Paval <ambroziepaval@Tapptitudes-Mac-mini.local>
2016-09-28 15:47:17 +0300

    Added drag&drop and swipe to delete.

--- app/src/main/java/com/tapptitude/weathertapp/MainActivity.java
+++ app/src/main/java/com/tapptitude/weathertapp/MainActivity.java
@@ -1,13 +1,17 @@
 package com.tapptitude.weathertapp;
 
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
     private RecyclerView mWeatherContentRV;
+    private FloatingActionButton mAddCardFAB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,9 +19,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mWeatherContentRV = (RecyclerView) findViewById(R.id.am_rv_dailyWeatherContent);
-        WeatherContentAdapter weatherAdapter = new WeatherContentAdapter(getApplicationContext());
+        final WeatherContentAdapter weatherAdapter = new WeatherContentAdapter(getApplicationContext());
         mWeatherContentRV.setAdapter(weatherAdapter);
         mWeatherContentRV.setLayoutManager(new LinearLayoutManager(this));
         mWeatherContentRV.setItemAnimator(new DefaultItemAnimator());
+
+        mAddCardFAB = (FloatingActionButton) findViewById(R.id.am_fab_addItem);
+        mAddCardFAB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                weatherAdapter.addNewItem();
+                mWeatherContentRV.scrollToPosition(weatherAdapter.getItemCount() - 1);
+            }
+        });
+
+
+        ItemTouchHelper.SimpleCallback simpleItemTouchCallback = new ItemTouchHelper.SimpleCallback(
+                ItemTouchHelper.DOWN | ItemTouchHelper.UP | ItemTouchHelper.START | ItemTouchHelper.END,
+                ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(RecyclerView recyclerView, RecyclerView.ViewHolder viewHolder, RecyclerView.ViewHolder target) {
+                recyclerView.getAdapter().notifyItemMoved(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+                weatherAdapter.swapItemsInList(viewHolder.getAdapterPosition(), target.getAdapterPosition());
+                return true;
+            }
+
+            @Override
+            public void onSwiped(RecyclerView.ViewHolder viewHolder, int direction) {
+                weatherAdapter.removeItem(viewHolder.getAdapterPosition());
+            }
+        };
+
+        ItemTouchHelper itemTouchHelper = new ItemTouchHelper(simpleItemTouchCallback);
+        itemTouchHelper.attachToRecyclerView(mWeatherContentRV);
     }
 }
--- app/src/main/java/com/tapptitude/weathertapp/WeatherContentAdapter.java
+++ app/src/main/java/com/tapptitude/weathertapp/WeatherContentAdapter.java
@@ -4,11 +4,16 @@ import android.content.Context;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.helper.ItemTouchHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,17 +23,20 @@ import com.tapptitude.weathertapp.weather.WeatherInfo;
 import com.tapptitude.weathertapp.weather.utils.TemperatureColorPicker;
 import com.tapptitude.weathertapp.weather.utils.json.DataReaderJSON;
 
-import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 /**
  * Created by ambroziepaval on 9/27/16.
  */
 public class WeatherContentAdapter extends RecyclerView.Adapter<WeatherContentAdapter.WeatherContentVH> {
+    private final Random random;
     private List<WeatherInfo> mWeatherInfoList;
+    private int lastPosition = -1;
 
     public WeatherContentAdapter(Context context) {
         this.mWeatherInfoList = DataReaderJSON.getWeatherData(context);
+        random = new Random();
     }
 
     @Override
@@ -39,18 +47,63 @@ public class WeatherContentAdapter extends RecyclerView.Adapter<WeatherContentAd
     }
 
     @Override
-    public void onBindViewHolder(WeatherContentVH holder, int position) {
+    public void onBindViewHolder(final WeatherContentVH holder, final int position) {
         WeatherInfo weatherInfo = mWeatherInfoList.get(position);
-        holder.mWeatherTempTV.setText(weatherInfo.mTemperature + " degrees Celsius");
+        holder.mWeatherTempTV.setText(weatherInfo.mTemperature + "° Celsius");
         holder.mWeatherConditionTV.setText(weatherInfo.mWeatherConditions.toString());
         holder.mWeatherSelectionTV.setText(((DailyWeatherInfo) weatherInfo).mDay);
         holder.mWeatherCardCV.setCardBackgroundColor(TemperatureColorPicker.getTemperatureColor(weatherInfo.mTemperature));
         holder.setConditionIcon(weatherInfo.mWeatherConditions);
+
+        setAnimation(holder.itemView, position);
+
+        holder.mRemoveCardB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int poz = holder.getAdapterPosition();
+                Animation animation = AnimationUtils.loadAnimation(v.getContext(), android.R.anim.slide_out_right);
+                v.startAnimation(animation);
+                lastPosition -= 1;
+                mWeatherInfoList.remove(poz);
+                notifyItemRemoved(poz);
+//                notifyItemRangeChanged(poz, mWeatherInfoList.size());
+            }
+        });
+    }
+
+    public void swapItemsInList(int position, int target){
+        WeatherInfo draggedCard = mWeatherInfoList.get(position);
+        mWeatherInfoList.remove(position);
+        mWeatherInfoList.add(target, draggedCard);
+    }
+
+    public void removeItem(int position){
+        mWeatherInfoList.remove(position);
+        notifyItemRemoved(position);
+//        notifyItemRangeRemoved(position, mWeatherInfoList.size());
+    }
+
+    private void setAnimation(View viewToAnimate, int position) {
+        if (position > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(viewToAnimate.getContext(), android.R.anim.slide_in_left);
+            viewToAnimate.startAnimation(animation);
+            lastPosition = position;
+        }
     }
 
     @Override
     public int getItemCount() {
         return mWeatherInfoList.size();
+    }
+
+    public void addNewItem() {
+        int temp = random.nextInt(40);
+        WeatherConditions weatherConditions = WeatherConditions.values()[random.nextInt(5)];
+        DailyWeatherInfo newInfo = new DailyWeatherInfo("NEW", temp, weatherConditions);
+        mWeatherInfoList.add(newInfo);
+        int size = mWeatherInfoList.size();
+        notifyItemInserted(size - 1);
+//        notifyItemRangeChanged(size - 1, size);
     }
 
     class WeatherContentVH extends RecyclerView.ViewHolder {
@@ -60,8 +113,9 @@ public class WeatherContentAdapter extends RecyclerView.Adapter<WeatherContentAd
         TextView mWeatherConditionTV;
         TextView mWeatherSelectionTV;
         ImageView mWeatherConditionImageIV;
+        Button mRemoveCardB;
 
-        public WeatherContentVH(View itemView) {
+        public WeatherContentVH(final View itemView) {
             super(itemView);
 
             mWeatherTempTV = (TextView) itemView.findViewById(R.id.ic_tv_weather_temp);
@@ -69,6 +123,7 @@ public class WeatherContentAdapter extends RecyclerView.Adapter<WeatherContentAd
             mWeatherConditionTV = (TextView) itemView.findViewById(R.id.ic_tv_weather_condition);
             mWeatherSelectionTV = (TextView) itemView.findViewById(R.id.ic_tv_weather_selection);
             mWeatherConditionImageIV = (ImageView) itemView.findViewById(R.id.ic_iv_weather_condition);
+            mRemoveCardB = (Button) itemView.findViewById(R.id.ic_b_remove);
             resources = itemView.getResources();
         }
 
